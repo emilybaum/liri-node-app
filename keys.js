ef5d6c95a7e0fef0893e6fb7b94668bb3bd68aad
emilybaum <emily.r.baum@gmail.com>
2019-06-29 15:21:29 -0400

    completed the spotify section

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('Spotify is loaded');
+console.log('Spotify is loaded on keys.js file');
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    secret: process.env.SPOTIFY_SECRET,
 };
--- liri.js
+++ liri.js
@@ -4,9 +4,10 @@ var keys = require("./keys.js");
 var axios = require('axios');
 var moment = require('moment');
 var inquirer = require('inquirer');
+var Spotify = require('node-spotify-api');
 
-// var spotify = new Spotify(keys.spotify);
-
+var command = process.argv[2]
+var whatToSearch = process.argv.slice(3).join(" ");
 
 // COMMANDS TO WRITE
     // concert-this
@@ -14,14 +15,52 @@ var inquirer = require('inquirer');
     // movie-this
     // do-what-it-says
 
+switch (command) {
+    case 'concert-this': console.log("concert-this selected");
+    break;
 
-// node liri.js concert-this '<_____>'
-// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    // Name of the venue
-    // Venue location
-    // Date of the Event(use moment to format this as "MM/DD/YYYY")
+    case 'spotify-this-song': console.log("spotify-this-song selected");
+        searchSpotify(whatToSearch);
+    break;
 
+    case 'movie-this': console.log("movie-this selected");
+    break;
 
+    case 'do-what-it-says': console.log("do-what-it-says selected");
+    break;
+}
+
+var divider = "\n--------------------------------------------------------\n"
+function searchSpotify(term) {
+    var spotify = new Spotify(keys.spotify);
+    console.log(term + " was searched");
+    spotify.search({ type: 'track', query: term }, function(err, response) {
+            if (err) {
+                return console.log(err);
+            }
+            var song = response.tracks.items[0];
+            var songName = song.name;
+            var artist = song.artists[0].name;
+            var preview = song.external_urls.spotify;
+            var album = song.album.name;
+
+            var details = [
+                "Song Name: " + songName, 
+                "Artist: " + artist, 
+                "Preview Link: " + preview, 
+                "Album: " + album,
+            ].join("\n");
+
+        fs.appendFile("logSong.txt", details + divider, (err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+
+        console.log(divider + details + divider)
+
+    });
+
+}
 // node liri.js spotify-this-song '<song name here>'
 // This will show the following information about the song in your terminal/bash window
     // Artist(s)
@@ -29,6 +68,14 @@ var inquirer = require('inquirer');
     // A preview link of the song from Spotify
     // The album that the song is from
     // If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+
+        
+// node liri.js concert-this '<_____>'
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // Name of the venue
+    // Venue location
+    // Date of the Event(use moment to format this as "MM/DD/YYYY")
 
 
 // node liri.js movie-this '<movie name here>'
@@ -46,17 +93,4 @@ var inquirer = require('inquirer');
 
 // node liri.js do-what-it-says
 // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-
-
-
-
-
-
-
-
-
-
-
-
 
